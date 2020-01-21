6c8cefffa3b73d726ceace0bc85191ee65f322e8
Oizys18 <oizys18@gmail.com>
2020-01-22 02:29:15 +0900

    Add links

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const shell = require("shelljs");
-
+const open = require('open');
 
 const init = () => {
 	console.log(
@@ -18,14 +18,16 @@ const init = () => {
 	);
 	console.log(
 		chalk.blueBright(
-			figlet.textSync("Resume", {
+			figlet.textSync("CLI", {
 				font: "Ghost",
 				horizontalLayout: "default",
 				verticalLayout: "default"
 			})
 		)
 	);
-	console.log("Welcome! This is Chanu CLI. Type '/help' for documentation.")
+
+	console.log(chalk.red('Welcome HPHK! This is Chanu CLI.'))
+	console.log(chalk.whiteBright(`Type 'help' for a list of commands.`))
 }
 
 const typeQuestions = () => {
@@ -33,7 +35,7 @@ const typeQuestions = () => {
 		{
 			type: "input",
 			name: "TYPE",
-			message: "What do you want to know about me?",
+			message: ">",
 		}
 	];
 	return inquirer.prompt(questions);
@@ -45,7 +47,7 @@ const askQuestions = () => {
 			type: "list",
 			name: "PICK",
 			message: "Pick one!",
-			choices: ["Basic_Information", "Notion_Resume", "Github_link", "Portfolio"],
+			choices: ["1.Basic_Information", "2.Notion_Resume", "3.Github_Page", "4.Portfolio", "5.Playlist"],
 			filter: function (val) {
 				return val.split(".")[1];
 			}
@@ -56,27 +58,55 @@ const askQuestions = () => {
 
 const run = async () => {
 	init();
-	while (true){
+	while (true) {
 		const typed_answer = await typeQuestions();
-		const {TYPE} = typed_answer
-		if (TYPE === 'help'){
-			console.log("This is documentation")
-		} 
-		else if (TYPE === 'hello'){
-			console.log("Hello there! Greetings!")
+		const { TYPE } = typed_answer
+		console.log()
+		if (TYPE === 'help') {
+			console.log("> help : Show a list of commands.\n" +
+				"> info : Basic information about me. \n" +
+				"> notion : Opens notion resume page on browser.\n" +
+				"> choices : Instead of typing command, show multiple choices.\n" +
+				"> hello : Send greetings to the developer.(Why not?)\n" +
+				"> bye : Exit the program.")
 		}
-		else if (TYPE === ''){
-			console.log("Type something PLZ!")
+		else if (TYPE === 'choices') {
+			console.log("Okay,then. Pick one!")
+			const picked_answer = await askQuestions();
+			console.log(picked_answer)
+			const { PICK } = picked_answer
+			if (PICK === 'Basic_Information') {
+				console.log("Hello! It's good to see you here!\n" +
+					"Thanks a lot for downloading this package.\n " +
+					"It means a lot to me.")
+			}
+			else if (PICK === 'Notion_Resume') {
+				await open("https://www.notion.so/devcatfall/d32421df0a9744fbb8aef2a43326a47f");
+			}
+			else if (PICK === 'Github_Page') {
+				await open("https://github.com/Oizys18");
+			}
+			else if (PICK === 'Portfolio') {
+				console.log('please add portfolio link')
+			}
+			else if (PICK === 'Playlist') {
+				await open("https://www.youtube.com/playlist?list=PLhtrNKyVEq0OC61zmSAeNErPxJeZJxZqm");
+			}
 		}
-		else if (TYPE === 'nah'){
-			console.log("Well, Suit yourself. :)")
+		else if (TYPE === 'hello') {
+			console.log("Hello! It's good to see you here!\n" +
+				"Thanks a lot for downloading this package.\n " +
+				"It means a lot to me.")
+		}
+		else if (TYPE === 'bye') {
+			console.log("Good bye! See you soon. :) ")
 			break
 		}
+		else if (TYPE === '') {
+			console.log("Type something PLZ!")
+		}
+		console.log()
 	}
-	
-	const picked_answer = await askQuestions();
-
 }
-
 
 run();
